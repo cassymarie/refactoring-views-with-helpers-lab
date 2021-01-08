535bd24a20d7f3a7a3d811a6166e5c2e1a06c5a8
cassymarie <cassy2205@gmail.com>
2021-01-08 14:50:25 -0700

    commit tree fb9fd09b85e9d318e7a88e007d32ead653b8f8c2

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,20 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
 
-  def artist_name
-  end
-
   def artist_name=(name)
-  end
+    self.artist = Artist.find_or_create_by(name: name)
+   end
+
+   def artist_name
+      self.artist ? self.artist.name : nil
+   end
+
+   def artist_link
+      if self.artist
+         artist_path(self.artist)
+      else
+         edit_song(self)
+      end
+   end
+   
 end
